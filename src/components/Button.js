7f6a6f60d927e4e0e80510f9c036cb15bee98ea0
Onyekachi Nwakaihe <kachi@Davids-MacBook-Pro.local>
2022-10-29 07:55:22 +0100

    Added Links

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,14 @@ import "./button.css";
 
 function Button({ name, link, id }) {
   return (
-    <button className="btn_profile py-6 w-full mb-6" id={id}>
+    <button
+      className="btn_profile py-6 w-full mb-6"
+      id={id}
+      onClick={(e) => {
+        window.open(link);
+        e.preventDefault();
+      }}
+    >
       {name}
     </button>
   );
