9497077961f1ddd0bbd8509fcdeb0b7fdac34d77
Onyekachi Nwakaihe <kachi@Davids-MacBook-Pro.local>
2022-10-31 07:38:16 +0100

    Added profile camera icon on hover

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@ import slackImg from "./assets/images/slack__img.svg";
 import githubImg from "./assets/images/github__img.svg";
 import zuriImg from "./assets/images/zuri__img.svg";
 import ingressiveImg from "./assets/images/ingressive__img.svg";
+import cameraIcon from "./assets/images/camera__icon.svg";
 import ReactTooltip from "react-tooltip";
 
 function App() {
   const [isShown, setIsShown] = useState(false);
+
+  console.log(isShown);
 
   return (
     <div className="App px-4 md:px-28 w-full">
@@ -25,6 +28,7 @@ function App() {
           >
             Share Link
           </ReactTooltip>
+          {/* Destop Share Link button */}
           <svg
             className="btn_share cursor-pointer"
             width="42"
@@ -64,9 +68,68 @@ function App() {
               strokeDasharray="1 3"
             />
           </svg>
-
+          {/* Mobile Share Link button */}
+          <svg
+            className="btn_share_mobile cursor-pointer"
+            width="42"
+            height="42"
+            viewBox="0 0 42 42"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            onMouseEnter={() => setIsShown(true)}
+            onMouseLeave={() => setIsShown(false)}
+          >
+            <rect
+              x="1"
+              y="1"
+              width="40"
+              height="40"
+              rx="20"
+              fill={isShown ? "#F9FAFB" : "white"}
+            />
+            <path
+              d="M21 21.8334C21.4602 21.8334 21.8333 21.4603 21.8333 21.0001C21.8333 20.5398 21.4602 20.1667 21 20.1667C20.5397 20.1667 20.1666 20.5398 20.1666 21.0001C20.1666 21.4603 20.5397 21.8334 21 21.8334Z"
+              stroke={isShown ? "#667085" : "#98A2B3"}
+              stroke-width="1.66667"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M26.8333 21.8334C27.2935 21.8334 27.6666 21.4603 27.6666 21.0001C27.6666 20.5398 27.2935 20.1667 26.8333 20.1667C26.3731 20.1667 26 20.5398 26 21.0001C26 21.4603 26.3731 21.8334 26.8333 21.8334Z"
+              stroke={isShown ? "#667085" : "#98A2B3"}
+              stroke-width="1.66667"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M15.1666 21.8334C15.6269 21.8334 16 21.4603 16 21.0001C16 20.5398 15.6269 20.1667 15.1666 20.1667C14.7064 20.1667 14.3333 20.5398 14.3333 21.0001C14.3333 21.4603 14.7064 21.8334 15.1666 21.8334Z"
+              stroke={isShown ? "#667085" : "#98A2B3"}
+              stroke-width="1.66667"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <rect
+              x="1"
+              y="1"
+              width="40"
+              height="40"
+              rx="20"
+              stroke="#D0D5DD"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-dasharray="1 3"
+            />
+          </svg>
           <article className="flex items-center justify-center">
-            <img src={profileImg} alt="profile__img" />
+            <div className="profile_container cursor-pointer">
+              <img
+                className="profile_img"
+                src={profileImg}
+                alt="profile__img"
+              />
+
+              <img className="camera_img" src={cameraIcon} alt="" />
+            </div>
           </article>
           <h1 className="profile_heading pt-6">Annette Black</h1>
         </header>
